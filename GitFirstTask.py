d2b389fef5e3255a0fe7939528711533d64eb876
Viktoriia Coskun <vicky.9995022@gmail.com>
2022-12-16 20:50:13 +0100

    Add GitFirstTask.py

--- GitFirstTask.py
+++ GitFirstTask.py
@@ -7,3 +7,7 @@ for store_name in shopping_list:
 	shopping_list[store_name]=[product.upper() for product in shopping_list[store_name]]
 	print("I'm going to",store_name.upper(),"i'm buying",shopping_list.get(store_name))
     
+products_together=0
+for keys in shopping_list:
+	products_together=products_together+len(shopping_list[keys])
+print("I bought",products_together,"products together")
